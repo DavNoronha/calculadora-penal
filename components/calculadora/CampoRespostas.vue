a66b465ec013af365a1fca1689357ff935790eaa
David Noronha <davidnd97@gmail.com>
2021-09-20 17:59:59 -0300

    cpage calculadora componentizada e responsiva

---
+++ components/calculadora/CampoRespostas.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-col :cols="$vuetify.breakpoint.smAndDown ? 12 : 7">
+    <v-card
+      :class="
+        $vuetify.breakpoint.smAndDown
+          ? 'mt-0 d-flex flex-column align-center justify-center'
+          : 'mt-12 d-flex flex-column align-center justify-center'
+      "
+    >
+      <v-card-title>
+        Resultado
+      </v-card-title>
+
+      <div class="mb-4">
+        <h3>
+          Final da pena: <span v-if="penaData != 'Invalid Date'">{{ dataFormatada }}</span>
+        </h3>
+      </div>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {
+  props: {
+    penaData: {
+      type: Date,
+      default: {}
+    },
+    dataFormatada: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
