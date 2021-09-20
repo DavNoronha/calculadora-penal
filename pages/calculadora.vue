d1f09dcaf87f60a1a5ec03164d776a8fe0e4dcd8
David Noronha <davidnd97@gmail.com>
2021-09-19 22:35:39 -0300

    soma de data-base e pena sendo exibida

--- pages/calculadora.vue
+++ pages/calculadora.vue
@@ -33,20 +33,9 @@
                 :label="item.nome"
                 outlined
               />
-              {{ item.valor }}
             </div>
-            {{ penaData }}
           </div>
         </div>
-
-        <v-btn
-          color="black--text success mb-12"
-          @click="calcular()"
-          large
-          rounded
-        >
-          Calcular
-        </v-btn>
       </v-card>
     </v-col>
 
@@ -58,14 +47,11 @@
           Resultado
         </v-card-title>
 
-        {{ resultadoData }}
-
-        <v-text-field
-          v-model="resultado"
-          label="Resultado"
-          type="tel"
-          outlined
-        />
+        <div class="mb-4">
+          <h3>
+            Final da pena: <span v-if="penaData != 'Invalid Date'">{{ dataFormatada }}</span>
+          </h3>
+        </div>
       </v-card>
     </v-col>
 
@@ -85,26 +71,32 @@ export default {
       dataBase: '',
       calculado: false,
       itensPena: [
-        { nome: 'ano', valor: null },
-        { nome: 'mes', valor: null },
-        { nome: 'dia', valor: null },
+        { nome: 'Anos', valor: null },
+        { nome: 'Meses', valor: null },
+        { nome: 'Dias', valor: null },
       ],
-      resultado: null,
     }
   },
   computed: {
-    resultadoData() {
-      return new Date(`${this.dataBase} 00:00:00`);
+    dataBaseData() {
+      let dataNormal = new Date(`${this.dataBase} 00:00:00`);
+      return dataNormal.getTime();//retorna data em milisegundos
     },
     penaData() {
-      let pena = `${this.itensPena[0].valor}-${this.itensPena[1].valor}-${this.itensPena[2].valor} 00:00:00`;
-      return console.log(new Date(pena))
+      let diaMiliAux = 1000*60*60*24;
+      let anosMili = diaMiliAux*365*this.itensPena[0].valor;
+      let mesesMili = diaMiliAux*30*this.itensPena[1].valor;
+      let diasMili = diaMiliAux*this.itensPena[2].valor;
+
+      let fimPena = new Date(this.dataBaseData+anosMili+mesesMili+diasMili);
+      return fimPena;
+    },
+    dataFormatada() {
+      let formatada =
+        `${this.penaData.getDate()}/${this.penaData.getMonth()+1}/${this.penaData.getFullYear()}`
+
+      return formatada;
     }
   },
-  methods: {
-    calcular() {
-      this.calculado = true;
-    }
-  }
 }
 </script>
